bf41b4d4dbb7106e37223f7eb2887a043dd51032
unknown <mrlnrose6@gmail.com>
2024-08-14 20:19:18 +0530

    removed tag

--- src/test/java/com/bdd/runnerfile/TestRunner.java
+++ src/test/java/com/bdd/runnerfile/TestRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:\\neworkspace\\BDD_Cucumber_July\\src\\test\\java\\com\\bdd\\featurefile",
 glue="com.bdd.stepdefinition",
-plugin= {"pretty","html:target/cucumber"},
+plugin= {"pretty","html:target/cucumber","json:target/cucumberr"},
 monochrome=true,//display the console out in a readable format
 dryRun=true)//To check the mapping between step definition and feature file to identify missing steps
 public class TestRunner {
